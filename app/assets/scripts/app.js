2a10c19e60a2b8cf21f388c55c01e0129509f845
frontpiotrend <frontpiotrend@gmail.com>
2017-08-17 14:19:47 +0200

    Completed sticky header functionality

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu(); //object which uses this class as a blueprint; saved to a variable
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 /* arguments above: 1) jquery-selector, 2) offset */
+var stickyHeader = new StickyHeader();
